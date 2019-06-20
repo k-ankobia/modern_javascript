28d02a65d717339601a5a8a167ab9f9f7fbf39d4
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-20 13:26:28 +0100

    converting primitive data types

---
+++ app.js
@@ -0,0 +1,21 @@
+let val 
+
+// Number to string 
+
+// val = String(5)  
+val = String(4+4233)  
+
+val = String(true)
+
+// Array to string
+val =  String([1,2,4,4,5,6])
+
+val = parseInt('100.30')
+val = parseFloat('100.30')
+
+
+
+console.log(val)
+console.log(typeof val)
+// console.log(val.length)
+console.log(val.toFixed())
