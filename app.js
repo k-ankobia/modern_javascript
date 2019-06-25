5ae2e79972143045be16ed10a472297deec45447
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-25 10:23:26 +0100

    new date added

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-// Object Literals
-const person =  {
-  firstName : 'steve',
-  lastName : 'john',
-  email : 'steve@gmail.com',
-  city : 'miami',
-  age : 30,
-  getBirthYear : function(){
-    return 2017 - this.age
-  },
-  hobbies : ['football', 'wrestling']
+// Date and time 
 
-}
+let val 
 
-val = person.hobbies[1]
-val = person.getBirthYear()
-
-
+const today = new Date()
+const birthday = new Date('9-10-1981 11:25:00')
+val = birthday 
 
 console.log(val)
 
-const people = [
-  {name: 'john', age: 30},
-  {name: 'mike', age: 33},
-  {name: 'nancy', age: 40}
-]
-
-for(let i = 0; i < people.length; i++) {
-  console.log(people[i].name)
-}
