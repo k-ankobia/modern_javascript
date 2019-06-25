8b108cec1c69b49a41c4fe89143a638621e68d02
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-25 15:33:43 +0100

    traversing the dom

--- app.js
+++ app.js
@@ -1,52 +1,15 @@
-// document.getElementsByClassName
+let val 
 
-// let items = document.getElementsByClassName('collection-item')
-// console.log(items[0])
-// items[0].style.color = 'red'
-// items[3].textContent = 'Hello World'
+const list = document.querySelector('ul.collection')
+const listItem = document.querySelector('li.collection-item:first-child')
 
+val = listItem
+val = list
 
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item')
+// GET CHILD NODES
+val = list.childNodes
 
-// console.log(listItems)
+// GET CHILDREN ELEMENT NODES 
+// val = list.children 
 
-//document.getElementByTagName
-
-// let lis = document.getElementsByTagName('li')
-// // console.log(lis[0])
-// lis[0].style.color = 'red'
-// lis[3].textContent = 'Hello World'
-
-// console.log(lis)
-// // Convert HTML Collection into array
-// lis = Array.from(lis)
-
-// lis.reverse()
-
-// lis.forEach(function(li) {
-//   console.log(li.className)
-//   li.textContent = 'Hello'
-// })
-// console.log(lis)
-
-// QUERYSELECTORALL
-// this produces a nodelist
-
-const items = document.querySelectorAll('ul.collection li.collection-item')
-console.log(items)
-
-items.forEach(function(item) {
-    item.textContent = 'Test'
-  })
-
-  const liOdd = document.querySelectorAll('li:nth-child(odd')
-  const liEven = document.querySelectorAll('li:nth-child(even')
-
-liOdd.forEach(function(li) {
-  li.style.background = '#ccc'
-})
-
-for(let i =0; i < liEven.length; i++){
-  console.log('test')
-  liEven[i].style.background = '#f4f4f4'
-}
+console.log(val) 
