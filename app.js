a83f68d966658f4c0ac17f67f2a1f07a254c861f
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-25 11:02:18 +0100

    function declerations

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 // Function Declartion with defaults
 
-function greet(firstname = 'john', lastname = 'Doe') {
-  return `Hello ${firstname} ${lastname}`
+// function greet(firstname = 'john', lastname = 'Doe') {
+//   return `Hello ${firstname} ${lastname}`
+// }
+
+// console.log(greet('richard', 'dickson'))
+
+
+// USING CONST TO DECLARE FUNCTIONS
+// const multiply = function(x = 2)  {
+//   return x * x
+// }
+// console.log(multiply())
+
+// ES6 DECLARATION 
+
+// const add = (x =2 ) => {
+//   return x + x 
+// }
+
+// console.log(add())
+
+
+// IMMEDIATELY INVOKABLE FUNCTION EPRESSIONS - IFFES
+
+// (function(name){
+//   console.log('IFFE Ran...' + name)
+// })('kwaku');
+
+// PROPERTY METHODS FOR OBJECTS
+
+const todo = {
+  add: function() {
+    console.log('Add todo...')
+  }, 
+  edit: function(id){
+    console.log(`Edit todo ${id}`)
+  }
 }
 
-console.log(greet('richard', 'dickson'))
+todo.delete = function() {
+  console.log('Delte todo...')
+}
+
+todo.add()
+todo.edit(2)
+todo.delete()
+
+console.log(todo)
