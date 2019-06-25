21a35d11ce6b6ee34e9d54b4be4ec95b36ddaea2
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-25 13:26:00 +0100

    Using querySelector() to change dom elements

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-// DOM 
-let val 
+// DOM SELECTORS
 
-val = window.document.head
-console.log(val)
+// document.getElementById()
+
+let val
+
+// val = document.getElementById('task-title')
+
+// const taskTitle = document.getElementById('task-title')
+
+// // changing styling
+// taskTitle.style.background = '#333'
+// taskTitle.style.color = '#fff'
+// taskTitle.style.padding = '5px'
+// // taskTitle('task-title').style.display = 'none'
+
+
+// // changning content 
+
+// taskTitle.textContent = 'Task List'
+// taskTitle.innerText = 'My Task'
+// taskTitle.innerHTML = '<span style ="color:red"> Task List </span>'
+
+// DOCUMENT.QUERYSELECTOR()
+
+
+// console.log(document.querySelector('#task-title'))
+// console.log(document.querySelector('.card-title'))
+// console.log(document.querySelector('h5'))
+
+document.querySelector('li').style.color = 'red'
+document.querySelector('li:last-child').style.color = 'blue'
+document.querySelector('li:nth-child(3)').style.color = 'green'
+document.querySelector('li:nth-child(4)').textContent = 'Hello World'
+document.querySelector('li:nth-child(odd)').style.background= '#ccc'
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4'
