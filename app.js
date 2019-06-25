6620047a17b93a6b50d2f6c38b2bb5fb89531226
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-06-25 12:46:07 +0100

    UI added for examining the DOM

--- app.js
+++ app.js
@@ -1,31 +1,5 @@
-//SCOPE 
+// DOM 
+let val 
 
-// global score 
-
-var a = 1 
-let b = 2
-const c = 3 
-
-// FUNCTION SCOPE
-// function test() {
-//   var a = 4 
-//   let b = 5
-//   const c = 6 
-//   console.log('Function scope: ', a, b, c)
-// }
-
-// test()
-
-// BLOCK SCOPE 
-// if(true) {
-//   // Block Scope 
-//   var a = 4 
-//   let b = 5
-//   const c = 6
-//   console.log('If scope: ', a, b, c)
-// }
-
-for(let a = 0; a < 10; a++) {
-  console.log(`${a}`)
-}
-console.log('Global scope: ', a, b, c)
+val = window.document.head
+console.log(val)
